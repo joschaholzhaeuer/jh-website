77357dfa42d0af4e16b33dceed68f1aab5ef9830
Joscha Holzhäuer <j.holzhaeuer@outlook.com>
2018-04-08 18:08:16 +0200

    added webmaster confirm page

---
+++ src/pages/google2b9be7855ef2e5e6.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Google2b9be7855ef2e5e6 extends React.Component {
+    render() {
+        return (
+            <div>
+                google-site-verification: google2b9be7855ef2e5e6.html
+            </div>
+        );
+    }
+}
+export default Google2b9be7855ef2e5e6;
