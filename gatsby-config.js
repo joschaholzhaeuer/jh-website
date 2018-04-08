2866cdf39068a42094f7a1dcd49becc1cf74b761
Joscha Holzhäuer <j.holzhaeuer@outlook.com>
2018-04-08 17:51:49 +0200

    added favicon and postcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,30 @@ module.exports = {
                 pathToConfigModule: `src/utils/typography.js`,
             },
         },
+        {
+            resolve: `gatsby-plugin-favicon`,
+            options: {
+                logo: "./src/favicon.png",
+                injectHTML: true,
+                icons: {
+                    android: true,
+                    appleIcon: true,
+                    appleStartup: true,
+                    coast: false,
+                    favicons: true,
+                    firefox: true,
+                    twitter: false,
+                    yandex: false,
+                    windows: true,
+                }
+            }
+        },
+        {
+            resolve: `gatsby-plugin-postcss-sass`,
+            options: {
+                precision: 8,
+            },
+        },
         `gatsby-plugin-sass`,
         `gatsby-plugin-react-helmet`,
     ],
